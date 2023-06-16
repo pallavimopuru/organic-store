e6c869f51fed9ec4290f453550d9df3bd11d9804
Pallavi Mopuru <mopurupallavi@gmail.com>
2023-06-16 11:33:07 +0530

    addproject-structure

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using organicstore.Models;
+using System.Diagnostics;
+
+namespace organicstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Medical()
+        {
+            return View();
+        }
+        public IActionResult Farming()
+        {
+            return View();
+        }
+        public IActionResult Sidebar()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+const navItems = document.querySelector('.navbar-nav');
+
+navItems.addEventListener('click', function (event) {
+    if (event.target.tagName === 'A') {
+        const links = navItems.querySelectorAll('.nav-link');
+        links.forEach(link => link.classList.remove('after-click'));
+        event.target.classList.add('after-click');
+        sessionStorage.setItem('clickedLinkId', event.target.id);
+    }
+});
+// Check if a link ID is stored in local storage
+const clickedLinkId = sessionStorage.getItem('clickedLinkId');
+if (clickedLinkId) {
+    const clickedLink = document.getElementById(clickedLinkId);
+    clickedLink.classList.add('after-click');
+} else {
+    const homelink = document.getElementById('homeLink');
+    homelink.classList.add('after-click');
+}
+
+
+//pop in array it will pop last element and return
+
+//push const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//fruits.push("Kiwi");  //5 return
+//fruits.pop();  //Mango
